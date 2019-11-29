6bdfb79e3b9065736fdabd1bbc4cc5acb4593de4
eduardocalix <eduardocalix11xtra@gmail.com>
2019-11-29 01:38:14 -0600

    componentes

--- currency/src/app/app.component.ts
+++ currency/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+//import font from '../assets/plugins/fontawesome-free/css/all.min.css';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','../assets/plugins/fontawesome-free/css/all.min.css'] 
 })
 export class AppComponent {
   title = 'currency';
--- currency/src/app/components/currency/currency.component.ts
+++ currency/src/app/components/currency/currency.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.css']
+  styleUrls: ['./currency.component.css','../../../assets/plugins/daterangepicker/daterangepicker.css','../../../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css','../../../assets/css/adminlte.min.css','../../../assets/plugins/jqvmap/jqvmap.min.css']
 })
 export class CurrencyComponent implements OnInit {
 
